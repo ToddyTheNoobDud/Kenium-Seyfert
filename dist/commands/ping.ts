9dc62ebf563c73f81c0e8314d7e2fe18109c3360
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-07-11 17:08:49 -0300

    Update ping.ts

--- dist/commands/ping.ts
+++ dist/commands/ping.ts
@@ -1,15 +1,27 @@
-import { Declare, Command, type CommandContext } from 'seyfert';
- 
+import { Embed, Declare, Command, type CommandContext } from "seyfert";
+
 @Declare({
   name: 'ping',
-  description: 'Show latency with Discord'
+  description: 'ping the bot',
 })
-export default class PingCommand extends Command {
-  async run(ctx: CommandContext) {
-    const ping = ctx.client.gateway.latency;
- 
-    await ctx.write({
-      content: `The latency is \`${ping}\``
-    });
-  }
+export default class pingCmds extends Command {
+    public override async run(ctx: CommandContext): Promise<void> {
+        const { client } = ctx;
+
+        const embed = new Embed()
+
+        await ctx.editOrReply({ embeds: [embed] });
+
+        const shardId = ctx.shardId;
+
+        const wsPing = Math.floor(client.gateway.latency);
+        const clientPing = Math.floor(Date.now() - (ctx.message ?? ctx.interaction)!.createdTimestamp);
+        const shardPing = Math.floor((await ctx.client.gateway.get(shardId)?.ping()) ?? 0);
+
+        embed
+            .setColor(0)
+            .setDescription(`**Gateway**: ${wsPing}ms\n**Client**: ${clientPing}ms\n**Shard**: ${shardPing}ms`);
+
+        await ctx.editOrReply({ embeds: [embed] });
+    }
 }
